a5bf46d8db176348e2e689b44155a6de0c34b7a3
harinik961 <harinik961@gmail.com>
2025-02-08 10:49:44 -0600

    Initial Commit

---
+++ CoverUp!/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoverUp!
+//
+//  Created by Harini Kolluru on 2/8/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
